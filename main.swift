149cae8ca5a412fef6f1cc6702e0fadbccd0cecb
whiteblueblack <zh13@Mac-29.local>
2016-03-10 10:24:37 +0800

    123
    123

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Lo1HELLoswift
+//
+//  Created by ZH13 on 16/3/7.
+//  Copyright © 2016年 ZH13. All rights reserved.
+//
+
+import Foundation
+
+var arr = ["hello","hahahahah",22,2.2]
+
+print(arr)
+
